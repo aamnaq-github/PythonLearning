4fe4309f89e14918b440861c2df5e7c9a35e0ff3
aamnaq-github <68397945+aamnaq-github@users.noreply.github.com>
2020-07-17 01:07:23 +0500

    Add files via upload

---
+++ Tutorial Exercises/Inheritance/Main.py
@@ -0,0 +1,8 @@
+from Inheritance.Chef import Chef
+from Inheritance.ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
